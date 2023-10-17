b96e646dafee2c143eee5205af4587ea292d2fda
zlx454545 <zlx454545@gmail.com>
2023-10-17 19:45:07 +0900

    Feat: 라우터 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import GlobalStyles from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
-    <div>
+    <>
       <GlobalStyles />
-      App
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 export default App;
